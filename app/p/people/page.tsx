e27790f1d4f01d9d073195a27e40be900fd8419b
de6eling <de6eling@gmail.com>
2024-12-26 09:36:36 -0700

    forgot to save

--- app/p/people/page.tsx
+++ app/p/people/page.tsx
@@ -1,15 +1,13 @@
 import Header from "@/components/shared/header";
 import { GroupListClient } from "@/components/group/group-list-client";
-import { getGroups } from "@/app/data/group";
 
 export default async function Page() {
-    const { groups, error } = await getGroups();
 
     return (
         <div className="w-full">
             <Header title="People" />
             <div className="max-w-4xl mx-auto p-6">
-                <GroupListClient groups={groups} />
+                <GroupListClient />
             </div>
         </div>
     );
