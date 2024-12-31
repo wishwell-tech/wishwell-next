dd69a2030e4d3e06bc397e240eacc88737e65693
tekel527 <dan@log.coach>
2024-12-31 14:04:12 -0700

    tweaking the prisma client and supabase redirects.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,24 +1,40 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined
+// }
 
-const prisma = globalForPrisma.prisma ?? 
-  new PrismaClient({
-    log: ['query', 'error', 'warn'],
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL ?? (() => {
-          console.error('DATABASE_URL is not set');
-          return 'invalid_url';
-        })()
-      },
-    },
-  })
+// const prisma = globalForPrisma.prisma ??
+//   new PrismaClient({
+//     log: ['query', 'error', 'warn'],
+//     datasources: {
+//       db: {
+//         url: process.env.DATABASE_URL ?? (() => {
+//           console.error('DATABASE_URL is not set');
+//           return 'invalid_url';
+//         })()
+//       },
+//     },
+//   })
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   globalForPrisma.prisma = prisma
+// }
 
-export { prisma } 
+// export { prisma }
+
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
