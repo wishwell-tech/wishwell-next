bacfa2ae6debc5cb8fa94a725b3d62dd33c63773
tekel527 <dan@log.coach>
2024-12-31 14:40:55 -0700

    finally figured it out

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -35,6 +35,6 @@ declare const globalThis: {
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default prisma;
+export { prisma };
 
 if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
